8a96d244bbacc1fa270d3addc7aec68e9c55afd6
thanhdat-30 <ngthanhdat.7019@gmail.com>
2024-12-03 01:45:05 +0700

    doan

--- src/main/java/com/viendong/webbanhang/exception/InternalServerErrorException.java
+++ src/main/java/com/viendong/webbanhang/exception/InternalServerErrorException.java
@@ -1,17 +1,17 @@
-package com.viendong.webbanhang.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class InternalServerErrorException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public InternalServerErrorException(String message) {
-        super(message);
-    }
-
-    public InternalServerErrorException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
+//package com.viendong.webbanhang.exception;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//
+//@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//public class InternalServerErrorException extends RuntimeException {
+//    private static final long serialVersionUID = 1L;
+//
+//    public InternalServerErrorException(String message) {
+//        super(message);
+//    }
+//
+//    public InternalServerErrorException(String message, Throwable cause) {
+//        super(message, cause);
+//    }
+//}
